e2b011dceac21dfbc9edb2e6b082b33f0f370247
Wesley Banfield <wesleybanfield@gmail.com>
2021-03-08 14:28:47 +0100

    Adding glob python files

--- panel_server/__init__.py
+++ panel_server/__init__.py
@@ -2,7 +2,7 @@ from glob import glob
 
 def setup_panel_server():
     return {
-        'command': ["panel", "serve", *glob('*.ipynb'), "--allow-websocket-origin=*", "--port", "{port}", "--prefix", "{base_url}panel"],
+        'command': ["panel", "serve", *glob('*.ipynb'), *glob('*.py'), "--allow-websocket-origin=*", "--port", "{port}", "--prefix", "{base_url}panel"],
         'absolute_url': True,
         'timeout': 360,
     }
