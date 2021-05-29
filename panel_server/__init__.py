f877564c26dd155e85f34094f53ddcf85721432b
Philipp Rudiger <prudiger@anaconda.com>
2021-05-29 13:14:36 +0200

    Serve .py files

--- panel_server/__init__.py
+++ panel_server/__init__.py
@@ -2,16 +2,16 @@ from glob import glob
 
 import param as _param
 
-def setup_panel_server():
-    return {
-        'command': ["panel", "serve", *glob('./**/*.ipynb', recursive=True), "--allow-websocket-origin=*", "--port", "{port}", "--prefix", "{base_url}panel", "--autoreload"],
-        'absolute_url': True,
-        'timeout': 360,
-    }
-
 __version__ = str(
     _param.version.Version(
         fpath=__file__,
         archive_commit="$Format:%h$",
-        reponame="panel_Server",
+        reponame="panel_server",
 ))
+
+def setup_panel_server():
+    return {
+        'command': ["panel", "serve", *glob('./**/*.ipynb', recursive=True), *glob('./**/*.py', recursive=True), "--allow-websocket-origin=*", "--port", "{port}", "--prefix", "{base_url}panel", "--autoreload"],
+        'absolute_url': True,
+        'timeout': 360,
+    }
