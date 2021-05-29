4d7acd1fb4f2d2375090ef9757d4a1a345b3f6df
Philipp Rudiger <prudiger@anaconda.com>
2021-05-29 02:37:07 +0200

    Serve notebooks recursively and autoreload

--- panel_server/__init__.py
+++ panel_server/__init__.py
@@ -2,7 +2,7 @@ from glob import glob
 
 def setup_panel_server():
     return {
-        'command': ["panel", "serve", *glob('*.ipynb'), "--allow-websocket-origin=*", "--port", "{port}", "--prefix", "{base_url}panel"],
+        'command': ["panel", "serve", *glob('./**/*.ipynb', recursive=True), "--allow-websocket-origin=*", "--port", "{port}", "--prefix", "{base_url}panel", "--autoreload"],
         'absolute_url': True,
         'timeout': 360,
     }
