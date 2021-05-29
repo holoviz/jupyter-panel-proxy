d08d1a6ac24165651fdb157ba38cbdddbc2093e3
Philipp Rudiger <prudiger@anaconda.com>
2021-05-29 03:15:09 +0200

    Fix tests

--- setup.py
+++ setup.py
@@ -2,17 +2,21 @@ import os
 
 import param
 
-import setuptools
+from setuptools import find_packages, setup
 
 
 extras_require = {
     'build': ['param >=1.7.0', 'setuptools'],
-    'tests': ['flake8'],
+    'tests': [
+        'flake8',
+        'twine',
+        'rfc3986',
+        'keyring'
+    ],
 }
 
-setuptools.setup(
+setup_args = dict(
     name="jupyter-panel-proxy",
-    packages=setuptools.find_packages(),
     description='Jupyter Server Proxy for Panel applications',
     version=param.version.get_setup_version(
         __file__,
@@ -21,7 +25,7 @@ setuptools.setup(
     ),
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    author= "Julia Signell",
+    author="Julia Signell",
     author_email= "developers@holoviz.org",
     maintainer= "HoloViz developers",
     maintainer_email= "developers@pyviz.org",
@@ -33,26 +37,31 @@ setuptools.setup(
     },
     platforms=['Windows', 'Mac OS X', 'Linux'],
     license='BSD',
-    entry_points={
-        'jupyter_serverproxy_servers': [
-            'panel = panel_server:setup_panel_server',
-        ]
-    },
-     classifiers = [
+    classifiers = [
         "License :: OSI Approved :: BSD License",
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Topic :: Scientific/Engineering",
-        "Topic :: Software Development :: Libraries"],
-    python_requires=">=2.7",
-    install_requires=['jupyter-server-proxy', 'panel >=2.3'],
+        "Topic :: Software Development :: Libraries"
+    ],
+    python_requires=">=3.6",
+    install_requires=['jupyter-server-proxy', 'panel >=0.11'],
     extras_require=extras_require,
+    packages=find_packages(),
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'panel = panel_server:setup_panel_server',
+        ]
+    },
 )
+
+if __name__ == '__main__':
+    setup(**setup_args)
