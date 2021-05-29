c8b167f1f45f13baad8fe2070e108e333d18a303
Philipp Rudiger <prudiger@anaconda.com>
2021-05-29 02:54:22 +0200

    Fix versioning

--- panel_server/__init__.py
+++ panel_server/__init__.py
@@ -1,8 +1,17 @@
 from glob import glob
 
+import param as _param
+
 def setup_panel_server():
     return {
         'command': ["panel", "serve", *glob('./**/*.ipynb', recursive=True), "--allow-websocket-origin=*", "--port", "{port}", "--prefix", "{base_url}panel", "--autoreload"],
         'absolute_url': True,
         'timeout': 360,
     }
+
+__version__ = str(
+    _param.version.Version(
+        fpath=__file__,
+        archive_commit="$Format:%h$",
+        reponame="panel_Server",
+))
--- setup.py
+++ setup.py
@@ -1,24 +1,9 @@
 import os
+
+import param
+
 import setuptools
 
-
-def get_setup_version(reponame):
-    """
-    Helper to get the current version from either git describe or the
-    .version file (if available).
-    """
-    import json
-    basepath = os.path.split(__file__)[0]
-    version_file_path = os.path.join(basepath, reponame, '.version')
-    try:
-        from param import version
-    except Exception:
-        version = None
-    if version is not None:
-        return version.Version.setup_version(basepath, reponame, archive_commit="$Format:%h$")
-    else:
-        print("WARNING: param>=1.6.0 unavailable. If you are installing a package, this warning can safely be ignored. If you are creating a package or otherwise operating in a git repository, you should install param>=1.6.0.")
-        return json.load(open(version_file_path, 'r'))['version_string']
 
 extras_require = {
     'build': ['param >=1.7.0', 'setuptools'],
@@ -29,7 +14,11 @@ setuptools.setup(
     name="jupyter-panel-proxy",
     packages=setuptools.find_packages(),
     description='Jupyter Server Proxy for Panel applications',
-    version=get_setup_version('panel_server'),
+    version=param.version.get_setup_version(
+        __file__,
+        "panel_server",
+        archive_commit="$Format:%h$",
+    ),
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     author= "Julia Signell",
